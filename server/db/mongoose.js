aebab0c6712e178c530325a8090c79ebed7adecb
Zehao Chen <adonis@Zehaos-MacBook-Pro.local>
2018-11-27 03:24:39 -0500

    Setup app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TestDB');
-mongoose.connect('mongodb://localhost:27017/pwdmanager');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pwdmanager');
 
 module.exports = {mongoose};
